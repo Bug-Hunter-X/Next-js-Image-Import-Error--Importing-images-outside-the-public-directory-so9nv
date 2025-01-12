e6f7ac1c5b961a52712b8182ec3412cd7a05eec8
BugHunter-X <askzenai@gmail.com>
2025-01-12 08:42:07 +0100

    Add aboutSolution.js file

---
+++ aboutSolution.js
@@ -0,0 +1,16 @@
+```javascript
+// pages/about.js
+function About() {
+  return (
+    <div>
+      <h1>About Page</h1>
+      <p>This is the about page.</p>
+      <img src="/images/my-image.jpg" alt="My Image" />
+    </div>
+  );
+}
+
+export default About;
+```
+
+**Note:** Create a directory named `images` inside the `public` directory and place your image in there.
